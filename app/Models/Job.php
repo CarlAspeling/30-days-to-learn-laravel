86ed4ee729ec9442567d4b676a30ea0a9af0a40d
Aspeling94 <carlaspeling@gmail.com>
2025-03-27 11:10:23 +0200

    Ensured fulliable set to protected for title and salary inputs.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
-
 
 class Job extends Model
 {
     protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
 }
