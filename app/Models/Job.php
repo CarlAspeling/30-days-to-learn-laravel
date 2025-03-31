ce51d34aff6847086dbea00a67491d706c8e5db5
Aspeling94 <carlaspeling@gmail.com>
2025-03-31 09:49:46 +0200

    Updated job model to ensure factory can be used and to specify relationship of job belonging to employer.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $fillable = ['employer_id', 'title', 'salary'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
