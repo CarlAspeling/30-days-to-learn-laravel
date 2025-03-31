61976d6abe3fca1dc3f58afa540874f3c2951fba
Aspeling94 <carlaspeling@gmail.com>
2025-03-31 09:47:53 +0200

    Added foreign ID to job listings migration to enable relationships (i.e., belongsTo).

--- database/migrations/2025_03_26_081259_create_job_listings_table.php
+++ database/migrations/2025_03_26_081259_create_job_listings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,8 +12,9 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignID('employer_id')->constrained()->cascadeOnDelete();
             $table->string('title');
-            $table->string('salary');
+            $table->decimal('salary', 10, 2);
             $table->timestamps();
         });
     }
