38158b678cec7a526f7af0a16020c9cb52a57099
Aspeling94 <carlaspeling@gmail.com>
2025-04-01 17:28:14 +0200

    Added error popups based on form input validation.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -21,6 +21,9 @@
                                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                        placeholder="Job title goes here">
                             </div>
+                            @error('title')
+                            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
 
@@ -33,10 +36,26 @@
                                        class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                        placeholder="Salary goes here">
                             </div>
+                            @error('salary')
+                            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
+                    {{--                <div class="mt-10">--}}
+                    {{--                    @if($errors->any())--}}
+                    {{--                        <ul>--}}
+                    {{--                            @foreach($errors->all() as $error)--}}
+                    {{--                                <li class="text-red-500 italic">--}}
+                    {{--                                    {{ $error }}--}}
+                    {{--                                </li>--}}
+                    {{--                            @endforeach--}}
+                    {{--                        </ul>--}}
+                    {{--                    @endif--}}
+                    {{--                </div>--}}
+
                 </div>
             </div>
+
         </div>
         </div>
 
@@ -47,5 +66,6 @@
                 Save
             </button>
         </div>
+
     </form>
 </x-layout>
