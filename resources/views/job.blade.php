78e62fad2fea8a2fb20a5025bfc734e056f6b436
Aspeling94 <carlaspeling@gmail.com>
2025-04-01 09:50:03 +0200

    Small change to individual job view to display currency correctly.

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -6,6 +6,6 @@
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
 
     <p>
-        This job pays {{ $job['salary'] }} per year.
+        This job pays ${{ number_format($job->salary, 2, '.', ',') }} per year.
     </p>
 </x-layout>
